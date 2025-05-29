faab4e163d99d513fdb490e5134ec74803afda5c
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-05-29 09:20:21 +0545

    DAO Added

--- DemoGame/src/demogame/util/DatabaseConnection.java
+++ DemoGame/src/demogame/util/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/demogame?useSSL=false";
-private static final String USER = "root"; // Your MySQL username
-private static final String PASSWORD = "Babita@000"; // Your MySQL password= "Babita@000"; // Replace with your MySQL password
+private static final String USER = "root"; // my MySQL username
+private static final String PASSWORD = "Babita@000"; //  MySQL password= "Babita@000"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
