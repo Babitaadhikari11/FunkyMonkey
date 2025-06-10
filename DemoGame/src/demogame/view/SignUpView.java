3d371759243ae52e98636c38cece82cd6dd3a780
GirishB-365 <girishbasnet96@gmail.com>
2025-06-10 18:02:28 +0545

    Updated with confirm password feild

--- DemoGame/src/demogame/view/SignUpView.java
+++ DemoGame/src/demogame/view/SignUpView.java
@@ -1,14 +1,14 @@
 package demogame.view;
 
 import demogame.util.RoundedPanel;
-
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class SignUpView extends JFrame {
     public JTextField usernameField = new JTextField();
     public JTextField emailField = new JTextField();
     public JPasswordField passwordField = new JPasswordField();
+    public JPasswordField confirmPasswordField = new JPasswordField(); // Added field
     public JCheckBox showPasswordCheck = new JCheckBox("Show Password");
     public JButton signUpButton = new JButton("Sign Up");
     public JLabel loginLink = new JLabel("<html><u>Already have an account? Login</u></html>");
@@ -30,11 +30,11 @@ public class SignUpView extends JFrame {
         background.setBounds(0, 0, 1200, 800);
         layeredPane.add(background, Integer.valueOf(0));
 
-        // Right Panel (moved lower)
+        // Right Panel
         RoundedPanel signUpPanel = new RoundedPanel(30);
         signUpPanel.setBackground(Color.WHITE);
         signUpPanel.setLayout(null);
-        signUpPanel.setBounds(720, 130, 400, 600);  // Changed y from 80 to 130
+        signUpPanel.setBounds(720, 130, 400, 600);
         layeredPane.add(signUpPanel, Integer.valueOf(1));
 
         JLabel title = new JLabel("Create Account");
@@ -46,20 +46,22 @@ public class SignUpView extends JFrame {
         addLabeledField(signUpPanel, "Username:", usernameField, 90);
         addLabeledField(signUpPanel, "Email:", emailField, 180);
         addLabeledField(signUpPanel, "Password:", passwordField, 270);
+        addLabeledField(signUpPanel, "Confirm Password:", confirmPasswordField, 360); // New
 
-        showPasswordCheck.setBounds(40, 350, 200, 30);
+        showPasswordCheck.setBounds(40, 440, 200, 30);
         showPasswordCheck.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-
         showPasswordCheck.addActionListener(e -> {
             if (showPasswordCheck.isSelected()) {
                 passwordField.setEchoChar((char) 0);
+                confirmPasswordField.setEchoChar((char) 0);
             } else {
                 passwordField.setEchoChar('•');
+                confirmPasswordField.setEchoChar('•');
             }
         });
-         signUpPanel.add(showPasswordCheck);
+        signUpPanel.add(showPasswordCheck);
 
-        signUpButton.setBounds(40, 400, 320, 50);
+        signUpButton.setBounds(40, 490, 320, 50);
         signUpButton.setBackground(Color.RED);
         signUpButton.setForeground(Color.WHITE);
         signUpButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
@@ -69,7 +71,7 @@ public class SignUpView extends JFrame {
 
         loginLink.setForeground(Color.BLUE);
         loginLink.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-        loginLink.setBounds(40, 470, 300, 30);
+        loginLink.setBounds(40, 560, 300, 30);
         loginLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         signUpPanel.add(loginLink);
     }
@@ -84,7 +86,8 @@ public class SignUpView extends JFrame {
         field.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
         panel.add(field);
     }
-    // getters
+
+    // Getters
     public String getUsername() {
         return usernameField.getText();
     }
@@ -96,7 +99,12 @@ public class SignUpView extends JFrame {
     public String getPassword() {
         return new String(passwordField.getPassword());
     }
-     public JButton getSignUpButton() {
+
+    public String getConfirmPassword() {
+        return new String(confirmPasswordField.getPassword());
+    }
+
+    public JButton getSignUpButton() {
         return signUpButton;
     }
 
