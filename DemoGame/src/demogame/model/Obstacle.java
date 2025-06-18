f4ba5526f472f30401b810b6e35752fcf56a940b
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-18 21:56:40 +0545

    different sizes obstacle creation tried

--- DemoGame/src/demogame/model/Obstacle.java
+++ DemoGame/src/demogame/model/Obstacle.java
@@ -7,6 +7,23 @@ public class Obstacle {
     private static final int VERTICAL_OFFSET = 40;
     private static final int MONKEY_DEFAULT_SIZE = 120;
     private static final int JUMP_CLEARANCE = 40;
+     // Size presets - correctly defined
+    private static final class SizePreset {
+        final int width;
+        final int height;
+        
+        SizePreset(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    private static final SizePreset[] SIZE_PRESETS = {
+        new SizePreset(60, 80),   // Small obstacle
+        new SizePreset(80, 100),  // Medium obstacle
+        new SizePreset(100, 120)  // Large obstacle
+    };
+
 
     
 }
