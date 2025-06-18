148ed6d052b31e82b417ad6d5ce8b24c9a7a61ba
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-18 21:55:39 +0545

    create physics constant for collision and speed

--- DemoGame/src/demogame/model/Obstacle.java
+++ DemoGame/src/demogame/model/Obstacle.java
@@ -1,5 +1,12 @@
 package demogame.model;
 import java.awt.Rectangle;
 public class Obstacle {
+    // Constants for obstacle properties
+    private static final int SPEED = 3;
+    private static final int COLLISION_INSET = 15;
+    private static final int VERTICAL_OFFSET = 40;
+    private static final int MONKEY_DEFAULT_SIZE = 120;
+    private static final int JUMP_CLEARANCE = 40;
+
     
 }
