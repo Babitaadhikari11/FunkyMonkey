328f2ea25a63c559ab568a6cd0a3867e1236a723
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-18 22:10:48 +0545

    animation frame array for each state

--- DemoGame/src/demogame/model/MonkeyState.java
+++ DemoGame/src/demogame/model/MonkeyState.java
@@ -1,5 +1,11 @@
 package demogame.model;
 
 public class MonkeyState {
+    // Animation frame ranges for each state
+    public static final int[] IDLE_FRAMES = {1, 2, 3, 4};
+    public static final int[] WALKING_FRAMES = {5, 6, 7, 8};
+    public static final int[] JUMPING_FRAMES = {9, 10, 11};
+    public static final int[] FALLING_FRAMES = {12, 13, 14};
+    public static final int VICTORY_FRAME = 16;
     
 }
