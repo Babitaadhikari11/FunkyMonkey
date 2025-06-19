4774e9b0cead5d6309f1ca62eb853bb75fa30e58
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-19 08:12:51 +0545

    add necessary getters and setters

--- DemoGame/src/demogame/model/Banana.java
+++ DemoGame/src/demogame/model/Banana.java
@@ -1,5 +1,22 @@
 package demogame.model;
-
+import java.awt.Rectangle;
 public class Banana {
+     private int x;
+    private int y;
+    private final int WIDTH = 30;
+    private final int HEIGHT = 30;
+    private boolean isCollected;
+    private Rectangle bounds;
+    private final int SPEED = 3;
+
+    // Getters and Setters
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return WIDTH; }
+    public int getHeight() { return HEIGHT; }
+    public boolean isCollected() { return isCollected; }
+    public void setCollected(boolean collected) { isCollected = collected; }
+    public Rectangle getBounds() { return bounds; }
+
     
 }
