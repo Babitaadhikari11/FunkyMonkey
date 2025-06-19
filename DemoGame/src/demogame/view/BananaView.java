d022db1af07d7dad086de07f82b11627b31c5efd
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-19 07:43:51 +0545

    add load banana image for loading banana from resource

--- DemoGame/src/demogame/view/BananaView.java
+++ DemoGame/src/demogame/view/BananaView.java
@@ -1,5 +1,24 @@
 package demogame.view;
-
+//import demogame.model.Banana;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 public class BananaView {
+       private Image bananaImage;
+    private final Color SCORE_COLOR = Color.WHITE;
+    private final Font SCORE_FONT = new Font("Arial", Font.BOLD, 20);
+
+    public BananaView() {
+        loadBananaImage();
+    }
+
+    private void loadBananaImage() {
+        try {
+            bananaImage = new ImageIcon(getClass().getResource("/resources/banana.png")).getImage();
+        } catch (Exception e) {
+            System.err.println("Error loading banana image: " + e.getMessage());
+        }
+    }
+
     
 }
