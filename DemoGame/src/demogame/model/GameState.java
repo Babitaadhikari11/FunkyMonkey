02eb65cf2e75fa5b27b649de2f7802563b4d5f11
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-19 09:33:00 +0545

    update

--- DemoGame/src/demogame/model/GameState.java
+++ DemoGame/src/demogame/model/GameState.java
@@ -1,6 +1,5 @@
 package demogame.model;
 
-
 public class GameState {
     private int score;
     private int level;
@@ -9,12 +8,14 @@ public class GameState {
     private float gameSpeed;
     private static final float SPEED_INCREMENT = 0.1f;
     private static final int LEVEL_UP_SCORE = 10;
+
     public GameState() {
         score = 0;
         level = 1;
         distanceTraveled = 0;
         gameSpeed = 1.0f;
     }
+
     public void update() {
         distanceTraveled++;
         // Level up every LEVEL_UP_SCORE points
@@ -22,11 +23,13 @@ public class GameState {
         // Increase game speed with level
         gameSpeed = 1.0f + (level - 1) * SPEED_INCREMENT;
     }
+
     // Getters and setters
     public int getScore() { return score; }
     public int getLevel() { return level; }
     public float getGameSpeed() { return gameSpeed; }
     public int getDistanceTraveled() { return distanceTraveled; }
+    
     public void addScore(int points) {
         score += points;
     }
