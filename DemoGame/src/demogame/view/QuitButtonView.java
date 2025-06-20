a37c8bd8f8d832cbed118d30122d4c3408ffc77a
paru-242326 <bistparas26@gmail.com>
2025-06-20 15:09:14 +0545

    add constants for button styling

--- DemoGame/src/demogame/view/QuitButtonView.java
+++ DemoGame/src/demogame/view/QuitButtonView.java
@@ -1,5 +1,18 @@
 package demogame.view;
+import java.awt.*;
+
 
 public class QuitButtonView {
+    // Constants for button styling
+    private static final int BUTTON_WIDTH = 120;
+    private static final int BUTTON_HEIGHT = 40;
+    private static final String BUTTON_TEXT = "Quit to Menu";
+    private static final String FONT_FAMILY = "Comic Sans MS";
+    private static final int FONT_SIZE = 14;
+    private static final Color TEXT_COLOR = Color.RED;
+    private static final Color BACKGROUND_COLOR = new Color(255, 255, 255, 200);
+    private static final Color HOVER_COLOR = new Color(255, 200, 200, 200);
+
+
     
 }
