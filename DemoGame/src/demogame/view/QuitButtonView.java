ecdaaafa2770cc2f9a80eb96bcf05188d725a350
paru-242326 <bistparas26@gmail.com>
2025-06-20 15:10:34 +0545

    add methods for styling quit button

--- DemoGame/src/demogame/view/QuitButtonView.java
+++ DemoGame/src/demogame/view/QuitButtonView.java
@@ -1,6 +1,8 @@
 package demogame.view;
 import java.awt.*;
 
+import javax.swing.BorderFactory;
+
 
 public class QuitButtonView {
     // Constants for button styling
@@ -12,6 +14,34 @@ public class QuitButtonView {
     private static final Color TEXT_COLOR = Color.RED;
     private static final Color BACKGROUND_COLOR = new Color(255, 255, 255, 200);
     private static final Color HOVER_COLOR = new Color(255, 200, 200, 200);
+     public QuitButtonView() {
+        initializeButton();
+        setupStyle();
+        addHoverEffect();
+    }
+     private void initializeButton() {
+        setText(BUTTON_TEXT);
+        setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+        setFocusPainted(false);
+        setBorderPainted(true);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+    }
+       
+    private void setupStyle() {
+        setFont(new Font(FONT_FAMILY, Font.BOLD, FONT_SIZE));
+        setForeground(TEXT_COLOR);
+        setBackground(BACKGROUND_COLOR);
+        
+        // Optional: Add border
+        setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createLineBorder(TEXT_COLOR, 1),
+            BorderFactory.createEmptyBorder(5, 10, 5, 10)
+        ));
+    }
+
+
+    
+
 
 
     
